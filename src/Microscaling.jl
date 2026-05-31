dd2c895f2218037c4e94ee89e0fd3ba2197b54d4
AntonOresten <antonoresten@proton.me>
2026-05-31 14:54:47 +0200

    Initial Microscaling commit

--- src/Microscaling.jl
+++ src/Microscaling.jl
@@ -1,5 +1,17 @@
-module Microscaling
+module Microscaling using Republic
 
-# Write your package code here.
+@reexport using Narrow
+@reexport using Microfloats
+@reexport using Blockscaling
+
+include("ScaleArray.jl")
+export ScaleArray
+public Dense, Sm1xx
+public relayout
+
+# DenseBlockscaledArray?
+
+const ColMajorBlockscaledMatrix{T,A<:BlockscaledMatrix{T,1}} = Union{PermutedDimsArray{T,2,(1,2),(1,2),A}, BlockscaledMatrix{T,1}}
+const RowMajorBlockscaledMatrix{T,A<:BlockscaledMatrix{T,1}} = PermutedDimsArray{T,2,(2,1),(2,1),A}
 
 end
